f884467cd36606ea1d953f9739c390f9785b222d
universo-isla <39147095+universo-isla@users.noreply.github.com>
2021-03-16 17:43:36 -0600

    Add files via upload

--- ImageClassifier.js
+++ ImageClassifier.js
@@ -1,9 +1,13 @@
-
+<div>Teachable Machine Image Model - p5.js and ml5.js</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/p5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/addons/p5.dom.min.js"></script>
+<script src="https://unpkg.com/ml5@latest/dist/ml5.min.js"></script>
+<script type="text/javascript">
   // Classifier Variable
   let classifier;
   // Model URL
-  let imageModelURL = 'https://teachablemachine.withgoogle.com/models/V2z_tjCdo/';
-
+  let imageModelURL = 'https://teachablemachine.withgoogle.com/models/H5urZ26hY/';
+  
   // Video
   let video;
   let flippedVideo;
@@ -16,10 +20,10 @@
   }
 
   function setup() {
-    createCanvas(640, 480);
+    createCanvas(320, 260);
     // Create the video
     video = createCapture(VIDEO);
-    video.size(640, 480);
+    video.size(320, 240);
     video.hide();
 
     flippedVideo = ml5.flipImage(video);
@@ -31,67 +35,13 @@
     background(0);
     // Draw the video
     image(flippedVideo, 0, 0);
-   
 
     // Draw the label
     fill(255);
     textSize(16);
     textAlign(CENTER);
     text(label, width / 2, height - 4);
-    print(label);
-      
-    if (label == 'Mi persona') {
-        image(flippedVideo, 0, 0);
-        //filter(THRESHOLD);
-            // Draw the label
-        fill(255);
-        textSize(16);
-        textAlign(CENTER);
-        text(label, width / 2, height - 4);
-        print(label);
-        print('pasaste por etiqueta1');
-    } else if(label == 'Guerrero jaguar'){
-        image(flippedVideo, 0, 0);
-        fill(255,0,0,100);
-        rect(0,0,640,480);
-        //filter(THRESHOLD);
-            // Draw the label
-        fill(255);
-        textSize(16);
-        textAlign(CENTER);
-        text(label, width / 2, height - 4);
-        print(label);
-        print('pasaste por etiqueta2');
-    }else if(label == 'Perrito de tela'){
-        image(flippedVideo, 0, 0);
-        fill(0,255,0,100);
-        rect(0,0,640,480);
-        //filter(INVERT);
-            // Draw the label
-        fill(255);
-        textSize(16);
-        textAlign(CENTER);
-        text(label, width / 2, height - 4);
-        print(label);
-        print('pasaste por etiqueta3');
-
-    }else if(label == 'Cubrebocas'){
-        image(flippedVideo, 0, 0);
-        fill(0,0,255,100);
-        rect(0,0,640,480);
-        //filter(INVERT);
-            // Draw the label
-        fill(255);
-        textSize(16);
-        textAlign(CENTER);
-        text(label, width / 2, height - 4);
-        print(label);
-        print('pasaste por etiqueta4');  
-        
-    }
-
   }
-
 
   // Get a prediction for the current video frame
   function classifyVideo() {
@@ -114,4 +64,4 @@
     // Classifiy again!
     classifyVideo();
   }
-
+</script>
